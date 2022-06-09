3760011e8e3e9b0f1ac916fc8f88541e86045832
kelmorsy <khaled.mg.a@gmail.com>
2022-06-09 19:23:28 +0200

    Modify todo populator behaviour to clear first

--- src/dom-modules/populateTodo.js
+++ src/dom-modules/populateTodo.js
@@ -1,9 +1,11 @@
 import drawCard from "./drawCard.js";
 
 const todoDOM = document.getElementsByTagName('main')[0];
+const newTodo = document.querySelector('.card.new-todo')
+console.log(newTodo)
 
 export default function populateTodo(project) {
-
+    todoDOM.innerHTML = '';
     for (let todo of project.todoList) {
         console.log(todo.state)
         if (todo.state.visibility) {
@@ -11,7 +13,7 @@ export default function populateTodo(project) {
             todoDOM.appendChild(drawCard(todo.id, todo.title, todo.desc,
                 todo.startDate, todo.endDate, todo.checklist,
                 todo.state.status))
-
         }
     }
+    todoDOM.appendChild(newTodo)
 }
