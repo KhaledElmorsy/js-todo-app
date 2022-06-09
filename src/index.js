f55c16c718d954ee697dfa1866c514ada3b9fea9
kelmorsy <khaled.mg.a@gmail.com>
2022-06-09 12:01:52 +0200

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -6,22 +6,5 @@ import makeEl from "./helper-modules/makeElement.js";
 import './assets/style.css'
 
 
-const projects = (!localStorage.projects) ? new projectList() : convertJSON(localStorage.projects);
-
-if (!projects._projects.length){
-    projects.addProject('Project 1')
-    projects.getProject(0).addTodo('Todo1','Todo1 Desc')
-    projects.getProject(0).state.toggleStatus();
-    projects.getProject(0).state.priority = 5;
-    projects.getProject(0).getTodo(0).checklist.addItem('Checklist Item1')
-    // projects.getProject(0).getTodo(0).state.disable()
-    projects.getProject(0).getTodo(0).checklist.getItem(0).state.toggleStatus()
-
-    projects.addProject('Project 2')
-    projects.getProject(1).state.disable();
-    projects.addProject('Project 3')
-    projects.addProject('Project 4')
-    localStorage.projects = JSON.stringify(projects)
-}
 
 DOM.pageLoad(projects)
