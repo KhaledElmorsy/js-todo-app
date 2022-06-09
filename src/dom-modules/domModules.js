2b005b8097f314dbc9f791b13dd1cdc5a6904798
kelmorsy <khaled.mg.a@gmail.com>
2022-06-09 17:44:38 +0200

    Add DOM populators to parent module

--- src/dom-modules/domModules.js
+++ src/dom-modules/domModules.js
@@ -1,7 +1,13 @@
 import pageLoad from './pageLoad.js'
+import populateProjects from './populateProjects.js'
+import populateTodo from './populateTodo.js'
+import drawCard from './drawCard.js'
 
 const DOM = {
-    pageLoad
+    pageLoad,
+    populateProjects,
+    populateTodo,
+    drawCard
 }
 
 export default DOM
--- src/dom-modules/pageLoad.js
+++ src/dom-modules/pageLoad.js
@@ -1,14 +1,11 @@
-import makeEl from "../helper-modules/makeElement.js";
-import populateTodo from "./populateTodo.js";
-import convertJSON from "../helper-modules/convertJSON.js";
-import populateProjects from "./populateProjects.js";
+import DOM from "./domModules.js";
 import projects from "../helper-modules/projectsInstance.js";
 
 export default function pageLoad() {
-    populateProjects();
+    DOM.populateProjects();
     
     let activeProjects = projects.getActive()
-    populateTodo(activeProjects[0]);
+    DOM.populateTodo(activeProjects[0]);
     
     return projects;
 }
