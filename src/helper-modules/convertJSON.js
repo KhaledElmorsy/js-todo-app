e569878c44c830e985c92a28a487e8253c2d62f6
kelmorsy <khaled.mg.a@gmail.com>
2022-06-09 06:32:32 +0200

    Add JSON import and conversion logic

---
+++ src/helper-modules/convertJSON.js
@@ -0,0 +1,36 @@
+import projectList from "../storage-classes/projectList";
+
+function importState(imported, object){
+    if (!imported._state.visible) object.state.disable();
+    if (imported._state.done) object.state.toggleStatus();
+    object.state.priority = imported._state._priority;
+}
+
+export default function convertJSON(projectJSON) {
+    const localSave = JSON.parse(projectJSON);
+    const projects = new projectList();
+
+    for (let project of localSave._projects) {
+        projects.add(project._name, project._id);
+
+        let currentProj = projects.getProject(project._id);
+        importState(project, currentProj);
+
+        if (project._todoList.length) {          
+            for (let todo of project._todoList) {
+                currentProj.add(todo._name, todo._desc,todo._startDate, todo._endDate, todo._id);
+                
+                let currentTodo = currentProj.getTodo(todo._id);
+                importState(todo, currentTodo);
+
+                if (todo._checklist.list.length) {
+                    for (let checklistItem of todo._checklist.list) {
+                        currentTodo.checklist.add(checklistItem._desc, checklistItem._id);
+                        importState(checklistItem, currentTodo.checklist.getItem(checklistItem._id));
+                    }
+                }
+            }
+        }
+    }
+    return projects
+}
