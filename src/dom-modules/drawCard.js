812aac2358542e45b331c24249ad3b193b5d9cea
kelmorsy <khaled.mg.a@gmail.com>
2022-06-10 07:32:01 +0200

    Move card template into an HTML template block and update card module

--- src/dom-modules/drawCard.js
+++ src/dom-modules/drawCard.js
@@ -1,16 +1,14 @@
 import makeEl from "../helper-modules/makeElement";
 
 export default function drawCard(id,title,desc,startDate,endDate,checklist,status){
-    const cardTemplate = document.getElementsByClassName('card-template')[0]
+    const cardTemplate = document.querySelector('#card-template').content.querySelector('.card')
     const card = cardTemplate.cloneNode(true);
 
     const cardTitle = card.getElementsByClassName('title')[0]
     const cardDesc = card.getElementsByClassName('desc')[0]
     const cardChecklist = card.getElementsByClassName('checklist-container')[0]
 
-    card.setAttribute('class','card')
     card.setAttribute('data-todo-id',id)
-    card.removeAttribute("hidden")
     if (status) card.classList.add('done')
     
     cardTitle.innerText = title;
