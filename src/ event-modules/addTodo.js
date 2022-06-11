50f20171dbd6c37f47f5497f9ec010baa4740e3a
kelmorsy <khaled.mg.a@gmail.com>
2022-06-11 22:19:10 +0200

    Save state to local storage after adding todo

--- src/ event-modules/addTodo.js
+++ src/ event-modules/addTodo.js
@@ -2,6 +2,8 @@ import DOM from "../dom-modules/domModules.js";
 import projects from "../helper-modules/projectsInstance.js";
 import elements from "../dom-elements/dom-elements.js";
 import activeObject from "../helper-modules/activeObject.js";
+import saveProjects from "../helper-modules/storage-modules/saveProjects.js";
+
 
 const projectsContainer = elements.containers.projectsContainer
 
@@ -20,7 +22,8 @@ export default function addTodo(){
         let checklistNodes = inputs['checklist'] // returns node list
         let checklist = [...checklistNodes].map(el=>el.value).filter(v=>v) // Convert to array, map to values and remove blanks
         
-        projects.getProject(activeProjID).addTodo(title,desc,undefined,undefined,undefined,checklist)
-        DOM.populateTodo(projects.getProject(activeProjID))
+        projects.getProject(projectID).addTodo(title,desc,undefined,undefined,undefined,checklist)
+        saveProjects()
+        DOM.populateTodo(projects.getProject(projectID))
     }   
 }
