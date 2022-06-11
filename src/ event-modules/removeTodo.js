73ae18d5a8f67bc92aafe947767ad1d9894fc89c
kelmorsy <khaled.mg.a@gmail.com>
2022-06-11 22:23:37 +0200

    Use DOM module instead of direct import

--- src/ event-modules/removeTodo.js
+++ src/ event-modules/removeTodo.js
@@ -1,9 +1,9 @@
-import populateTodo from "../dom-modules/populateTodo";
+import DOM from "../dom-modules/domModules.js";
 import projects from "../helper-modules/projectsInstance.js";
 import activeObject from "../helper-modules/activeObject";
 
 export default function removeTodo(todoID){
     let projectID = activeObject.projectID
     projects.getProject(projectID).getTodo(todoID).state.disable();
-    populateTodo(projects.getProject(projectID))
+    DOM.populateTodo(projects.getProject(projectID))
 }
