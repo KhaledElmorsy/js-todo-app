47744ac53bd73cbf416b28586f174604275c7f28
kelmorsy <khaled.mg.a@gmail.com>
2022-06-15 19:51:03 +0200

    Update and improve constructors

--- src/model/index.js
+++ src/model/index.js
@@ -2,9 +2,9 @@
  * Objects in the model will be instances of these classes.
  * The instances are nested by type: Project List > Project > Todo > Checklist Item
  * Each parent stores one or more child instances in an array.
- * Each class also has properties that define their state: status, visibility
+ * Each class also has properties that define their state: status, visible
  *  - status: Stores whether the item is completed or not. 
- *  - visibility: To maintain history and simpler ID assignment logic, instances
+ *  - visible: To maintain history and simpler ID assignment logic, instances
  *       aren't deleted from the children array when 'removed' by the client.
  *       Their visibility is set to false, and the view object won't render them.
  *       This has the added benefit of implementing recovery functionality, and
@@ -12,39 +12,39 @@
  */
 
 class ChecklistItem {
-    constructor(id, descr, status = false, visibility = true) {
+    constructor(id, descr) {
         this.id = id;
         this.descr = descr;
 
-        this.status = status;
-        this.visibility = visibility;
+        this.status = false;
+        this.visible = true;
     }
 }
 
 class Todo {
-    constructor(id, name, descr, endDate = null, startDate = null, status = false, visibility = true) {
-        this.id = id;
+    constructor(id, name, descr, endDate = null, startDate = null, list = null) {
+    this.id = id;
         this.name = name;
         this.descr = descr;
         this.endDate = endDate;
         this.startDate = startDate;
 
-        this.status = status;
-        this.visibility = visibility;
+        this.status = false;
+        this.visible = true;
 
-        this.checklist = []
+        this.list = list? list:[]
     }
 }
 
 class Project {
-    constructor(id, name, status = false, visibility = true) {
+    constructor(id, name, list = null) {
         this.id = id;
         this.name = name;
 
-        this.status = status;
-        this.visibility = visibility;
+        this.status = false;
+        this.visible = true;
 
-        this.todoList = []
+        this.list = list? list : []
     }
 }
 
