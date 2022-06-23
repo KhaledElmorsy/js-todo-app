b5abe5b34391bc2a860bad9167c2b3d458b0c38a
kelmorsy <khaled.mg.a@gmail.com>
2022-06-23 13:35:17 +0200

    Extract out common parent model class

--- src/model.js
+++ src/model.js
@@ -1,49 +1,35 @@
-/***
- * Objects in the model will be instances of these classes.
- * The instances are nested by type: Project List > Project > Todo > Checklist Item
- * Each parent stores one or more child instances in an array.
- * Each class also has properties that define their state: status, visible
- *  - status: Stores whether the item is completed or not. 
- *  - visible: To maintain history and simpler ID assignment logic, instances
- *       aren't deleted from the children array when 'removed' by the client.
- *       Their visibility is set to false, and the view object won't render them.
- *       This has the added benefit of implementing recovery functionality, and
- *       caveats can be easily mitigated with a clearing and renumbering function
- */
 
-class ChecklistItem {
-    constructor(id, descr) {
+class ObjectModel {
+    constructor(id){
         this.id = id;
-        this.descr = descr;
-
         this.status = false;
         this.visible = true;
     }
 }
 
-class Todo {
+
+class ChecklistItem extends ObjectModel {
+    constructor(id, descr) {
+        super(id);
+        this.descr = descr;
+    }
+}
+
+class Todo extends ObjectModel {
     constructor(id, title, descr, endDate = null, startDate = null, list = null) {
-    this.id = id;
+        super(id);
         this.title = title;
         this.descr = descr;
         this.endDate = endDate;
         this.startDate = startDate;
-
-        this.status = false;
-        this.visible = true;
-
         this.list = list? list:[]
     }
 }
 
-class Project {
+class Project extends ObjectModel {
     constructor(id, title, list = null) {
-        this.id = id;
+        super(id);
         this.title = title;
-
-        this.status = false;
-        this.visible = true;
-
         this.list = list? list : []
     }
 }
