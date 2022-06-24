60a7cfc6b0312808d079e8522c37487867541cef
kelmorsy <khaled.mg.a@gmail.com>
2022-06-24 15:40:00 +0200

    Fix documentaion @return values

--- src/templates/helpers.js
+++ src/templates/helpers.js
@@ -2,17 +2,16 @@
  * Returns a string based on the model's status that can be added to a view element's class list
  * @param {Object} model - Model object being checked
  * @param {string} htmlClass - string to be returned if the model's status is 'true'
- * @returns {string} - htmlClass parameter value if Todo's status is true, '' if false.
+ * @returns {string} - HTML class name if Todo's status is true, blank ('') if false.
  */
 function statusToClass(model, htmlClass) {
     return model.status ? htmlClass : '';
 }
-    
- // Checklist Item Functions
+ /**@typedef {import('../model.js').DataModel} DataModel*/   
  /**
   * Filters child objects based on their visibility
   * @param {Array} list - Array of child objects in the parent model
-  * @returns {Array} - Filtered array
+  * @returns {DataModel[]} - Array of data model subclass objects (Project, Todo, Checklist Item)
   */
  function getVisible(list) { return list.filter(item => item.visible)}
 
