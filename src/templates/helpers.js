e7fc7da7185110431341031a4c1983732aa1ac77
kelmorsy <khaled.mg.a@gmail.com>
2022-06-24 22:01:37 +0200

    Add helpers to outer namespace

--- src/templates/helpers.js
+++ src/templates/helpers.js
@@ -1,12 +1,13 @@
 /**
- * @namespace templateHelpers
+ * @namespace TemplateHelpers
+ * @memberof Templates
  * @property {Function} statusToClass Return a string based on a model object's status flag
  * @property {Function} getActive Return
  */
 
 /**
  * Returns a string based on the model's status that can be added to a view element's class list
- * @memberof templateHelpers
+ * @memberof Templates.TemplateHelpers
  * @param {Object} model - Model object being checked
  * @param {string} htmlClass - string to be returned if the model's status is 'true'
  * @returns {string} - HTML class name if Todo's status is true, blank ('') if false.
@@ -17,7 +18,7 @@ function statusToClass(model, htmlClass) {
  /**@typedef {import('../model.js').DataModel} DataModel*/   
  /**
   * Filters child objects based on their visibility
-  * @memberof templateHelpers
+  * @memberof Templates.TemplateHelpers
   * @param {Array} list - Array of child objects in the parent model
   * @returns {DataModel[]} - Array of data model subclass objects (Project, Todo, Checklist Item)
   */
