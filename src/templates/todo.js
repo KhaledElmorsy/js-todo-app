24b15bd6073525b0474b0677d6720d1158bfdb56
kelmorsy <khaled.mg.a@gmail.com>
2022-06-24 15:05:05 +0200

    Restructure and add documentation

--- src/templates/todo.js
+++ src/templates/todo.js
@@ -1,44 +1,56 @@
 import { statusToClass } from "./helpers"
+
 /**
  * Template for the element that shows the Todo item in more detail.
- * Is loaded by the view when you click the todo item.
+ * Is rendered by the view when you click the todo item.
+ * 
+ * {@link todoTemplates Visit Module}
+ * @namespace todoTemplates
+ * @property {Function} standard Create Outer HTML for the standard Todo View Element {@link todoTemplates.standard see here}
  */
-
-// Generate HTML template literal using the project object
-const standardTodoDetails = (todoObj) => {
-
-    const elementHTML =
-    `<div id="todo-background">
-        <form id="todo-details" class="${statusToClass(todoObj,'done')}">
-            <input required class="title" name="title" value="${todoObj.title}">
-            <textarea class="descr" name="descr">${todoObj.descr}</textarea>
-            <div class="checklist-container">
-                <h3>Tasks</h3>
-                <div id="checklist"></div>
-            </div>
-            <div class="dates">
-                <div class="date-container">
-                    <p class="date-type">Start Date</p>
-                    <input type="date" name="startDate" value="${todoObj.startDate || ''}">
-                </div>
-                <div class="date-container">
-                    <p class="date-type">End Date</p>
-                    <input type="date" name="endDate" value="${todoObj.endDate || ''}">
-                </div>
-            </div>
-            <div class="button-container">
-                <button class="save button" type="submit" name="save">Save</button>
-                <div class="done-toggle button">Done</div>
-                <div class="delete button">Delete</div>
-            </div>
-        </form>
-    </div>`
-    return elementHTML
-}
-
-
 const todoTemplates = {
-    standard: standardTodoDetails,
+    /**
+     * Todo model object 
+     * @typedef {import ('../model.js').Todo} Todo
+     */
+
+    /**
+     * Generate HTML for a modal element using a todo object model. The modal displays 
+     * properties of the Todo item in more detail.
+     * 
+     * {@link todoTemplates.standard View}
+     * @param {Todo} todoObj Todo object to fill modal values with
+     * @returns {string} Element Outer HTML
+     */
+    standard(todoObj){
+        const elementHTML =
+            `<div id="todo-background">
+                <form id="todo-details" class="${statusToClass(todoObj,'done')}">
+                    <input required class="title" name="title" value="${todoObj.title}">
+                    <textarea class="descr" name="descr">${todoObj.descr}</textarea>
+                    <div class="checklist-container">
+                        <h3>Tasks</h3>
+                        <div id="checklist"></div>
+                    </div>
+                    <div class="dates">
+                        <div class="date-container">
+                            <p class="date-type">Start Date</p>
+                            <input type="date" name="startDate" value="${todoObj.startDate || ''}">
+                        </div>
+                        <div class="date-container">
+                            <p class="date-type">End Date</p>
+                            <input type="date" name="endDate" value="${todoObj.endDate || ''}">
+                        </div>
+                    </div>
+                    <div class="button-container">
+                        <button class="save button" type="submit" name="save">Save</button>
+                        <div class="done-toggle button">Done</div>
+                        <div class="delete button">Delete</div>
+                    </div>
+                </form>
+            </div>`
+        return elementHTML
+    }
 }
 
 export default todoTemplates
