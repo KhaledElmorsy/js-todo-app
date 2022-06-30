ed7457a1030f7136760a9a67ff48ae72dbe32595
kelmorsy <khaled.mg.a@gmail.com>
2022-06-30 21:31:12 +0200

    Add jsdoc to webpack output
    Add hyperlink to jsdoc output (/out) in index.html template and use copy-webpack-plugin to copy the output to dist

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin')
 
 const isProduction = process.env.NODE_ENV == 'production';
 
@@ -24,6 +25,11 @@ const config = {
         new HtmlWebpackPlugin({
             template: 'index.html',
         }),
+        new CopyPlugin({
+            patterns: [
+                { from: "out", to: "out" },
+            ]
+        })
     ],
     module: {
         rules: [
